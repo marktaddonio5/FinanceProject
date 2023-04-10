7249cc75695445d07049310e3c79fc6134314b33
marktaddonio5 <marktaddonio5@gmail.com>
2023-04-10 14:07:02 +0200

    Figured out a way to access parts of api data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import SearchBar from "./components/SearchBar";
+import SearchBar from './components/SearchBar';
 import StockQuote from './components/StockQuote';
 
 /* Add a way to save certain stocks */
 
 function App() {
+
+
+
+
+  
   return (
     <div className="App">
       <header className="App-header">Finance Project</header>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,9 +5,11 @@ import { useState, useEffect, useRef } from "react";
 const SearchBar = () => {
     const [ticker, setTicker] = useState("")
     const [search, setSearch] = useState("")
-    const [data, setData] = useState();
-    const [loading, setLoading] = useState();
-    const [error, setError] = useState();
+    const [data, setData] = useState("");
+    const [meta, setMeta] = useState("")
+    const [metaObj, setMetaObj] = useState('')
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     let url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${ticker}&apikey=SDBO2DT8VQU27H16`;
 
@@ -25,13 +27,38 @@ const SearchBar = () => {
                     }
                     return response.json();
                 })
-                .then((data) => console.log(data))
+                .then((jsondata) => {
+                   /* const jsonString = JSON.stringify(jsondata);
+                    setData(jsonString); */
+                    console.log(Object.entries(jsondata))
+                    setData(Object.entries(jsondata))
+                })
                 .catch((err) => {
                     console.log(err.message);
+                    setError(err.message);
+                    setData(null)
+                })
+                .finally(() => {
+                    setLoading(false)
                 })
         }
     }, [url]);
 
+    useEffect(() => {
+        console.log('effect2 ran')
+        if (initialRender.current === false) {
+            console.log(data[0])
+            setMeta(data[0])
+        } 
+    }, [data]);
+
+    useEffect(() => {
+        console.log('effect3 ran')
+        if (initialRender.current === false) {
+            console.log(meta[1])
+            setMetaObj(meta[1])
+        } 
+    }, [meta]);
 
     function handleSubmit(e){
         e.preventDefault();
@@ -41,14 +68,16 @@ const SearchBar = () => {
     }
 
     return (
+        <>
         <form onSubmit={handleSubmit} style={{padding: "10px"}}>
             <input value={search} onChange={(e) => setSearch(e.target.value)} type="text" name="search" id="search" placeholder="Enter stock ticker symbol" style={{width: "40vw"}} />
             <button type="submit" style={{width: "20vw"}}>Search</button>
-            <span>Search: {search}</span>
-            <span>Ticker: {ticker}</span>
-            <span>InitialRender? {initialRender.current.toString()}</span>
         </form>
-        
+        <span>Search: {search}</span>
+        <span>Ticker: {ticker}</span>
+        <span>InitialRender? {initialRender.current.toString()}</span>
+        <span>{metaObj[ '2. Symbol' ]}</span>
+        </>
     );
 };
 
